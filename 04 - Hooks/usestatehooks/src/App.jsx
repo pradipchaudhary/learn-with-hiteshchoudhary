d06d2c71641b256c421456c0bef6c70249e81371
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-08-27 22:15:11 +0545

    useState hooks practice.

--- 04 - Hooks/usestatehooks/src/App.jsx
+++ 04 - Hooks/usestatehooks/src/App.jsx
@@ -1,24 +1,13 @@
-import { useState } from "react";
 import "./App.css";
+import ClassUseEffect from "./components/ClassUseEffect";
+import FunctionalUseEffect from "./components/FunctionalUseEffect";
 
 function App() {
-    const [count, setCount] = useState(0);
-    function increaseCount() {
-        return setCount(count + 1);
-    }
-    function decreaseCount() {
-        if (count !== 0) return setCount(count - 1);
-    }
     return (
         <>
-            <div>
-                <h1>Count : {count}</h1>
-                <button type="button" onClick={increaseCount}>
-                    INCREASE{" "}
-                </button>
-                <button type="button" onClick={decreaseCount}>
-                    DECREASE
-                </button>
+            <div className="useEffect">
+                <ClassUseEffect />
+                <FunctionalUseEffect />
             </div>
         </>
     );
