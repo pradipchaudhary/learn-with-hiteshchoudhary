42a9e90094ad4d816e8a1edbb47913841d05cf61
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-10-12 19:49:54 +0545

    counter component.

---
+++ 02counter/src/components/Count.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Count = () => {
+    const count = 0;
+    return (
+        <>
+            <div className="count">
+                <p>Count :</p>
+                <h1>{count}</h1>
+            </div>
+        </>
+    );
+};
+
+export default Count;
