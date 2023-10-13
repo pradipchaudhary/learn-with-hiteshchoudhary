e261a19d599194a00a2c57b6071518c0b0b15914
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-10-13 23:12:51 +0545

    counter component

--- 02counter/src/components/Count.jsx
+++ 02counter/src/components/Count.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Count = () => {
-    const count = 0;
+    // const count = 0;
+    const [count, setCount] = useState(10);
+    const increaseCounter = () => {
+        console.log("increase conuter " + count);
+        setCount(count + 1);
+    };
     return (
         <>
             <div className="count">
-                <p>Count :</p>
+                <h2>Count </h2>
                 <h1>{count}</h1>
+                <button type="button" onClick={increaseCounter}>
+                    Increase{" "}
+                </button>
             </div>
         </>
     );
