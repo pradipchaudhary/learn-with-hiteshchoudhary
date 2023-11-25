6a9751e6017b952c7b75986741a14cfac105d9d0
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-11-26 01:57:55 +0545

    reconciliation in react.

--- 03tailwindcss/src/App.jsx
+++ 03tailwindcss/src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div className="container p-2">
-                <h1 className="text-4xl border-solid border-b-orange-300 text-green-400">
+            <div className="container p-2 border-b m-auto border-gray-300 pb-4">
+                <h1 className="text-4xl border-solid border-b-orange-300  text-green-400 hover:text-gray-500">
                     Tailwind CSS
                 </h1>
             </div>
--- README.md
+++ README.md
@@ -18,3 +18,13 @@ link: [Repository](https://github.com/codezri/react-router-v6-example/tree/main)
 8. React Router
 9. Content API
 10. Redux toolkit
+
+## Reconciliation
+
+\_React is creating the virtual representation of your UI in the memory, and then ReactDOM receives that and syncs your UI (and the changes to it) to the DOM. This process is called reconciliation.
+
+# Recap
+
+-   ReactDOM is the glue between React and the DOM.
+-   ReactDOM is separate from React because you can write React for native applications.
+-   Reconciliation is the process of syncing the Virtual DOM to the actual DOM.
