fcbe0ce167a1da677a425158157115078fc0988a
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-12-06 21:27:04 +0545

    react project 04bgChanger added.

---
+++ 04bgChanger/src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <div className="fixed bottom-10 bg-gray-800 p-4 rounded-xl min-w-[800px] mx-auto">
+            Navigation
+        </div>
+    );
+};
+
+export default Navigation;
