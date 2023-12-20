864c9c72ac2db09b7acd5b11848f7b40c69c1117
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-12-20 12:46:15 +0545

    update object state using useState hook.

--- 05hooks/src/hooks/UseState_Hooks.jsx
+++ 05hooks/src/hooks/UseState_Hooks.jsx
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const UseState_Hooks = () => {
     const [count, setCount] = useState(0);
 
+    const [person, setPerson] = useState({
+        firstName: "John",
+        lastName: "Smith",
+        age: 20,
+    });
+
     const increaseHandler = () => {
-        setCount(count + 1);
+        setCount((pre) => pre + 4);
+        setCount((pre) => pre + 1);
+    };
+
+    const updatePersonhandler = () => {
+        setPerson((prevState) => {
+            return { ...prevState, age: 30 };
+        });
     };
     return (
         <div>
             <h2>UseState_Hooks</h2>
             <h1>{count}</h1>
-            <br />
             <button onClick={increaseHandler}>Increase </button>
+
+            <br />
+            <h4>Person Detail</h4>
+            <p>
+                Hi my name is {person.firstName} {person.lastName}, and age is :{" "}
+                {person.age}{" "}
+            </p>
+            <button onClick={updatePersonhandler}>Update Person Detail</button>
         </div>
     );
 };
