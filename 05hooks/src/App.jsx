c43b773706b5553f504de996ae256049c7d334bc
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-12-21 19:03:08 +0545

    added tailwind css .

--- 05hooks/src/App.jsx
+++ 05hooks/src/App.jsx
@@ -1,9 +1,11 @@
+// import UseEffect from "./hooks/UseEffect";
 import UseState from "./hooks/UseState";
 function App() {
     return (
         <>
             <div>
                 <UseState />
+                {/* <UseEffect /> */}
             </div>
         </>
     );
--- 05hooks/src/hooks/UseState.jsx
+++ 05hooks/src/hooks/UseState.jsx
@@ -1,17 +1,60 @@
 import { useState } from "react";
 const UseState = () => {
-    // Hooks
-    const [name, setName] = useState("Pradip");
-    //     const [myArr, setMyArr] = useState([2, 4, 6, 8]);
+    // Objects
+    const [state, setState] = useState({ name: "John", age: 30 });
+
+    // Arrays
+    const [array, setArray] = useState([1, 2, 3, 4, 5]);
+
+    const updateName = () => {
+        return {
+            ...state,
+            name: "Pradip",
+        };
+    };
+
+    const updateAge = () => {
+        return {
+            ...state,
+            age: state.age + 1,
+        };
+    };
 
     return (
-        <div>
+        <div className="container mx-auto m-5">
             {/* String */}
-            <h1>Hold String</h1>
+            <h1 className="text-4xl font-bold mb-4">useState hooks </h1>
             <hr />
-            <div>
-                <p>{name}</p>
-            </div>
+            <h4 className="text-xl font-semibold my-4">Object</h4>
+            <p>
+                Name: {state.name}, Age : {state.age}
+            </p>
+            <button
+                className="p-2 border m-2"
+                onClick={() => setState(updateName)}
+            >
+                Update Name
+            </button>
+            <button
+                className="p-2 border m-2"
+                onClick={() => setState(updateAge)}
+            >
+                Update Age
+            </button>
+
+            {/* Array */}
+            <h4 className="text-xl font-semibold my-4"> Array</h4>
+            <ul className="mb-2">
+                {array.map((arr, i) => {
+                    return <li key={i}> {arr} </li>;
+                })}
+            </ul>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Add Arr
+            </button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Remove Arr
+            </button>
         </div>
     );
 };
