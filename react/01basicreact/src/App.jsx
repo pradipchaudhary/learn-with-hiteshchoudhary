61454de653026822ef3be0b9b2fa8737e9c867a6
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-01-24 14:57:13 +0545

    TypeScript fundamentals

--- react/01basicreact/src/App.jsx
+++ react/01basicreact/src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Home from "./home";
-import ClassComponent from "./components/ClassComponent";
 
 const App = () => {
     return (
         <div>
-            <h1>App</h1>
-            {/* <Home /> */}
-            <ClassComponent />
+            <h1>Basic React </h1>
         </div>
     );
 };
