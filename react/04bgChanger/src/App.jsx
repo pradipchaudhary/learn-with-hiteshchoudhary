1aff6b0f5e0bbea33b0af049fd9c8508ad36cb08
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-11-09 18:53:17 +0545

    fixed bgChanger bug

--- react/04bgChanger/src/App.jsx
+++ react/04bgChanger/src/App.jsx
@@ -1,11 +1,30 @@
-import Navigation from "./components/Navigation";
+import { useState } from "react";
+import { colors } from "./colors";
 
-function App() {
-    return (
-        <>
-            <Navigation />
-        </>
+const App = () => {
+    const [bgColor, setBgColor] = useState(
+        `linear-gradient(to top, #d5dee7 0%, #ffafbd 0%, #c9ffbf 100%)`
     );
-}
+
+    return (
+        <div
+            className={`w-full h-screen overflow-hidden flex justify-center items-center`}
+            style={{ background: `${bgColor}, fixed` }}
+        >
+            <div className="fixed bottom-10 flex space-x-2 p-4 px-10 bg-slate-200 rounded-[25px]">
+                {colors.map((color, index) => (
+                    <button
+                        key={index}
+                        onClick={() => {
+                            setBgColor(color);
+                        }}
+                        className="w-8 h-8 border rounded-full border-none"
+                        style={{ background: `${color}, fixed` }}
+                    ></button>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
