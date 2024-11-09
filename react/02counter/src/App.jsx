e15b24618a2e6029498d2551f80ce2b14b433a4b
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-11-09 15:47:23 +0545

    React state handling

--- react/02counter/src/App.jsx
+++ react/02counter/src/App.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import Count from "./components/Count";
 
 const App = () => {
     return (
-        <div className="h-screen w-full bg-gray-700 flex justify-center items-center">
+        <div className="h-screen w-full bg-gray-200 flex justify-center items-center">
             <Count />
         </div>
     );
--- react/02counter/src/components/Count.jsx
+++ react/02counter/src/components/Count.jsx
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Count = () => {
     // const count = 0;
     const [count, setCount] = useState(10);
     const increaseCounter = () => {
-        console.log("increase conuter " + count);
         setCount(count + 1);
+    };
+
+    const increaseByTwo = () => {
+        setCount((prvCount) => prvCount + 1);
+        setCount((prvCount) => prvCount + 1);
+    };
+
+    const reset = () => {
+        setCount(10);
     };
     return (
         <>
             <div className="text-center">
-                <h1 className="text-5xl font-bold text-white">{count}</h1>
-                <button
-                    className="bg-green-700  text-white font-bold py-3 px-7 mt-8 rounded uppercase"
-                    type="button"
-                    onClick={increaseCounter}
-                >
-                    Increase{" "}
-                </button>
+                <h1 className="text-6xl font-bold text-black mb-7">{count}</h1>
+                <div className="flex gap-10">
+                    <button
+                        className="bg-green-800  text-white font-bold py-3 px-7 mt-8 rounded uppercase"
+                        type="button"
+                        onClick={increaseCounter}
+                    >
+                        Increase{" "}
+                    </button>
+                    <button
+                        className="bg-purple-800  text-white font-bold py-3 px-7 mt-8 rounded uppercase"
+                        onClick={increaseByTwo}
+                    >
+                        Increase By Two{" "}
+                    </button>
+
+                    <button
+                        className="bg-slate-800  text-white font-bold py-3 px-7 mt-8 rounded uppercase"
+                        onClick={reset}
+                    >
+                        Reset
+                    </button>
+                </div>
             </div>
         </>
     );
